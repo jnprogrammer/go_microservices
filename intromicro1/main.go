f26b50af5a85fba2ecde46c02d2c6feb40b59bda
jnprogrammer <joshuanparker@gmail.com>
2020-07-23 18:42:22 -0400

    Learning how Mux works and playing with http.Handlers and logging

--- intromicro1/main.go
+++ intromicro1/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("GoING FAST BOOIII!!!!  !! ")
+	})
+
+	http.HandleFunc("/money", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Go and give me your MOANAYAAYA !! ")
+	})
+
 	http.ListenAndServe(":8710", nil)
 }
