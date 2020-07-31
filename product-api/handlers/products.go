c88d77a64d32031e1023e9431ce197015c013453
jnprogrammer <joshuanparker@gmail.com>
2020-07-30 23:37:23 -0400

    Added updateProduct functionality

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -53,7 +53,8 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
 			return
 		}
-		p.l.Println("got id", id)
+		p.updateProducts(id, rw, r)
+		return
 	}
 
 	//catch all for not impalement handlers
@@ -78,4 +79,25 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	data.AddProduct(prod)
+}
+
+func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle update product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	if err == data.UpdateProduct(id, prod) {
+		if err == data.ErrProductNotFound {
+			http.Error(rw, "Product not found", http.StatusInternalServerError)
+		}
+		return
+	}
+	if err != nil {
+		http.Error(rw, "internal error is found", http.StatusInternalServerError)
+		return
+	}
 }
